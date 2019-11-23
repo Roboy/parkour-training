69a0fc1b58c59274b107f86a1e3753651692ff01
Alexander Koch <alexander@4koch.de>
2019-11-23 15:55:38 +0100

    add load of humanoid model

--- gym_parkour/envs/parkour_gym.py
+++ gym_parkour/envs/parkour_gym.py
@@ -1,10 +1,12 @@
 import numpy as np
 import gym
 from gym import spaces
+from gym_parkour.envs.bullet_environment import BulletEnvironment
 
 
 class ParkourGym(gym.Env):
     def __init__(self, *args, **kwargs):
+        self.bullet = BulletEnvironment()
         action_low = np.array([-1, -1, -1])
         observation_low = np.array([-2, -2, -2])
         self.action_space = spaces.Box(action_low, -action_low)
