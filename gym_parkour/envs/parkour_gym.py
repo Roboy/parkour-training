e35573d76b349234a70a33a469e19212f5efe3e6
Alexander Koch <alexander@4koch.de>
2019-11-23 16:22:48 +0100

    change to directly using robot_bases

--- gym_parkour/envs/parkour_gym.py
+++ gym_parkour/envs/parkour_gym.py
@@ -2,11 +2,16 @@ import numpy as np
 import gym
 from gym import spaces
 from gym_parkour.envs.bullet_environment import BulletEnvironment
-
+from gym_parkour.envs.robot_bases import MJCFBasedRobot
+import pybullet as p
 
 class ParkourGym(gym.Env):
     def __init__(self, *args, **kwargs):
-        self.bullet = BulletEnvironment()
+        physics_client = p.connect(p.GUI)
+        robot_model_path = '/home/alex/parkour-training/gym_parkour/envs/assets/humanoid.xml'
+        p.setGravity(0, 0, -10)
+        self.robot = MJCFBasedRobot(robot_model_path, robot_name='', action_dim=21, obs_dim=21)
+        self.robot.reset(p)
         action_low = np.array([-1, -1, -1])
         observation_low = np.array([-2, -2, -2])
         self.action_space = spaces.Box(action_low, -action_low)
