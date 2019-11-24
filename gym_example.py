570a42cfb4762258be404228337d8cb7e1ba2476
Alexander Koch <alexander@4koch.de>
2019-11-24 14:22:03 +0100

    add running infinite number of episodes

--- gym_example.py
+++ gym_example.py
@@ -1,6 +1,17 @@
 import gym_parkour
 import gym
+import time
 
-env = gym.make('ParkourChallenge-v0')
-obs = env.reset()
-print(obs)
+env = gym.make('ParkourChallenge-v0', render=True)
+while True:
+    done = False
+    obs = env.reset()
+
+    while not done:
+        action = env.action_space.sample()
+        obs, reward, done, info = env.step(action)
+        print(obs)
+        env.render(mode='human')
+        time.sleep(0.52)
+
+time.sleep(10)
