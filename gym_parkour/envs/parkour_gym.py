f407383b5c503fed192ea132021f9ae94f096aa0
Alexander Koch <alexander@4koch.de>
2019-11-24 13:23:56 +0100

    add necessary file for stadium scene

--- gym_parkour/envs/parkour_gym.py
+++ gym_parkour/envs/parkour_gym.py
@@ -1,7 +1,9 @@
 from pybulletgym.envs.roboschool.envs.locomotion.walker_base_env import WalkerBaseBulletEnv
 from pybulletgym.envs.roboschool.robots.locomotors import Humanoid
 # from pybulletgym.envs.roboschool.robots.locomotors import HumanoidFlagrun, HumanoidFlagrunHarder
-from gym_parkour.envs.stadium import StadiumScene
+from gym_parkour.envs.stadium_scene import StadiumScene
+from gym_parkour.envs.track_scene import TrackScene
+from gym_parkour.envs.wall_scene import WallScene
 from gym_parkour.envs.humanoid_flagrun import HumanoidFlagrun
 
 
@@ -15,7 +17,7 @@ class ParkourGym(WalkerBaseBulletEnv):
         self.stall_torque_cost = 4.25 * WalkerBaseBulletEnv.stall_torque_cost
 
     def create_single_player_scene(self, bullet_client):   # useless ?
-        scene = StadiumScene(bullet_client, gravity=9.8, timestep=0.0165/4, frame_skip=4)
+        scene = TrackScene(bullet_client, gravity=9.8, timestep=0.0165/4, frame_skip=4)
         WalkerBaseBulletEnv.stadium_scene = scene
         # s = WalkerBaseBulletEnv.create_single_player_scene(self, bullet_client)
         scene.zero_at_running_strip_start_line = False
---
+++ gym_parkour/envs/track_scene.py
@@ -0,0 +1,11 @@
+import os
+from gym_parkour.envs.stadium_scene import StadiumScene
+
+
+class TrackScene(StadiumScene):
+    def episode_restart(self, bullet_client):
+        # let parent object load plane
+        StadiumScene.episode_restart(self, bullet_client)
+        track_filename = os.path.join(os.path.dirname(__file__), "assets", "track.urdf")
+        self._p.loadURDF(track_filename, useFixedBase=1, basePosition=(7, 0, 0), baseOrientation=(-1, 0, 0, -1))
+
