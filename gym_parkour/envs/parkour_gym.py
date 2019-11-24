ff90bc0506067f5cb3cf8bdff7055ce216d91849
Alexander Koch <alexander@4koch.de>
2019-11-24 13:01:18 +0100

    copied files from pybulletgym since pip install of pybullet gym does not download assets

--- gym_parkour/envs/parkour_gym.py
+++ gym_parkour/envs/parkour_gym.py
@@ -1,6 +1,8 @@
 from pybulletgym.envs.roboschool.envs.locomotion.walker_base_env import WalkerBaseBulletEnv
 from pybulletgym.envs.roboschool.robots.locomotors import Humanoid
-from pybulletgym.envs.roboschool.robots.locomotors import HumanoidFlagrun, HumanoidFlagrunHarder
+# from pybulletgym.envs.roboschool.robots.locomotors import HumanoidFlagrun, HumanoidFlagrunHarder
+from gym_parkour.envs.stadium import StadiumScene
+from gym_parkour.envs.humanoid_flagrun import HumanoidFlagrun
 
 
 class ParkourGym(WalkerBaseBulletEnv):
@@ -12,7 +14,9 @@ class ParkourGym(WalkerBaseBulletEnv):
         self.electricity_cost = 4.25 * WalkerBaseBulletEnv.electricity_cost
         self.stall_torque_cost = 4.25 * WalkerBaseBulletEnv.stall_torque_cost
 
-    # def create_single_player_scene(self, bullet_client):   # useless ?
-    #     s = WalkerBaseBulletEnv.create_single_player_scene(self, bullet_client)
-    #     s.zero_at_running_strip_start_line = False
-    #     return s
+    def create_single_player_scene(self, bullet_client):   # useless ?
+        scene = StadiumScene(bullet_client, gravity=9.8, timestep=0.0165/4, frame_skip=4)
+        WalkerBaseBulletEnv.stadium_scene = scene
+        # s = WalkerBaseBulletEnv.create_single_player_scene(self, bullet_client)
+        scene.zero_at_running_strip_start_line = False
+        return scene
