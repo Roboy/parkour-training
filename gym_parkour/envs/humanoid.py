010021fa5eaf49fa038b1fedbbfe55b21c31ad17
Alexander Koch <alexander@4koch.de>
2019-11-30 17:59:43 +0100

    moved general reset from humanoid to parkour_robot

--- gym_parkour/envs/humanoid.py
+++ gym_parkour/envs/humanoid.py
@@ -10,7 +10,7 @@ class Humanoid(ParkourRobot, MJCFBasedRobot):
     self_collision = True
     foot_list = ["right_foot", "left_foot"]  # "left_hand", "right_hand"
 
-    def __init__(self, random_yaw = False, random_lean=False):
+    def __init__(self, random_yaw=False, random_lean=False):
         ParkourRobot.__init__(self)
         MJCFBasedRobot.__init__(self, 'humanoid_symmetric.xml', 'torso', action_dim=17, obs_dim=44)
         # 17 joints, 4 of them important for walking (hip, knee), others may as well be turned off, 17/4 = 4.25
@@ -22,8 +22,8 @@ class Humanoid(ParkourRobot, MJCFBasedRobot):
 
     def robot_specific_reset(self, bullet_client):
         ParkourRobot.robot_specific_reset(self, bullet_client)
-        self.motor_names  = ["abdomen_z", "abdomen_y", "abdomen_x"]
-        self.motor_power  = [100, 100, 100]
+        self.motor_names = ["abdomen_z", "abdomen_y", "abdomen_x"]
+        self.motor_power = [100, 100, 100]
         self.motor_names += ["right_hip_x", "right_hip_z", "right_hip_y", "right_knee"]
         self.motor_power += [100, 100, 300, 200]
         self.motor_names += ["left_hip_x", "left_hip_z", "left_hip_y", "left_knee"]
@@ -34,15 +34,15 @@ class Humanoid(ParkourRobot, MJCFBasedRobot):
         self.motor_power += [75, 75, 75]
         self.motors = [self.jdict[n] for n in self.motor_names]
         if self.random_yaw:
-            position = [0,0,0]
-            orientation = [0,0,0]
+            position = [0, 0, 0]
+            orientation = [0, 0, 0]
             yaw = self.np_random.uniform(low=-3.14, high=3.14)
             if self.random_lean and self.np_random.randint(2) == 0:
                 if self.np_random.randint(2) == 0:
-                    pitch = np.pi/2
+                    pitch = np.pi / 2
                     position = [0, 0, 0.45]
                 else:
-                    pitch = np.pi*3/2
+                    pitch = np.pi * 3 / 2
                     position = [0, 0, 0.25]
                 roll = 0
                 orientation = [roll, pitch, yaw]
@@ -53,33 +53,12 @@ class Humanoid(ParkourRobot, MJCFBasedRobot):
             self.robot_body.reset_orientation(p.getQuaternionFromEuler(orientation))
         self.initial_z = 0.8
 
+    # overwrite ParkourRobot
     def apply_action(self, a):
-        assert(np.isfinite(a).all())
+        assert (np.isfinite(a).all())
         force_gain = 1
         for i, m, power in zip(range(17), self.motors, self.motor_power):
             m.set_motor_torque(float(force_gain * power * self.power * np.clip(a[i], -1, +1)))
 
     def alive_bonus(self, z, pitch):
-        return +2 if z > 0.78 else -1   # 2 here because 17 joints produce a lot of electricity cost just from policy noise, living must be better than dying
-
-
-    def reset(self, bullet_client):
-        full_path = os.path.join(os.path.dirname(__file__), "assets", self.model_xml)
-
-        self._p = bullet_client
-        # print("Created bullet_client with id=", self._p._client)
-        if self.doneLoading == 0:
-            self.ordered_joints = []
-            self.doneLoading = 1
-            if self.self_collision:
-                self.objects = self._p.loadMJCF(full_path,
-                                                flags=pybullet.URDF_USE_SELF_COLLISION | pybullet.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS)
-                self.parts, self.jdict, self.ordered_joints, self.robot_body = self.addToScene(self._p, self.objects)
-            else:
-                self.objects = self._p.loadMJCF(full_path)
-                self.parts, self.jdict, self.ordered_joints, self.robot_body = self.addToScene(self._p, self.objects)
-        self.robot_specific_reset(self._p)
-
-        s = self.calc_state()  # optimization: calc_state() can calculate something in self.* for calc_potential() to use
-
-        return s
+        return +2 if z > 0.78 else -1  # 2 here because 17 joints produce a lot of electricity cost just from policy noise, living must be better than dying
--- gym_parkour/envs/parkour_robot.py
+++ gym_parkour/envs/parkour_robot.py
@@ -1,24 +1,96 @@
+import os
+import pybullet
+from pybulletgym.envs.roboschool.robots.robot_bases import XmlBasedRobot
 import numpy as np
-from pybulletgym.envs import gym_utils as ObjectHelper
-from pybulletgym.envs.roboschool.robots.locomotors.walker_base import WalkerBase
 
 
-class ParkourRobot(WalkerBase):
+class ParkourRobot(XmlBasedRobot):
     def __init__(self):
-        WalkerBase.__init__(self, power=0.41)
-        self.walk_target_x = 15
+        power = 0.41
+        self.power = power
+        self.camera_x = 0
+        self.start_pos_x, self.start_pos_y, self.start_pos_z = 0, 0, 0
+        self.walk_target_x = 1e3  # kilometer away
         self.walk_target_y = 0
-        self.flag = None
-        # self.target_index = 0
-        # self.targets = [(0, 0), (18, 0.0), (27, -2), (37, 3), (45, -3)]
+        self.body_xyz = [0, 0, 0]
 
     def robot_specific_reset(self, bullet_client):
+        self._p = bullet_client
         # self.flag = ObjectHelper.get_sphere(self._p, self.walk_target_x, self.walk_target_y, 0.7) # can cause reset error
-        WalkerBase.robot_specific_reset(self, bullet_client)
+        for j in self.ordered_joints:
+            j.reset_current_position(self.np_random.uniform(low=-0.1, high=0.1), 0)
+
+        self.feet = [self.parts[f] for f in self.foot_list]
+        self.feet_contact = np.array([0.0 for f in self.foot_list], dtype=np.float32)
+        try:
+            self.scene.actor_introduce(self)
+        except AttributeError:
+            pass
+        self.initial_z = None
+
+    def apply_action(self, a):
+        assert (np.isfinite(a).all())
+        for n, j in enumerate(self.ordered_joints):
+            j.set_motor_torque(self.power * j.power_coef * float(np.clip(a[n], -1, +1)))
 
     def calc_state(self):
-        state = WalkerBase.calc_state(self)
-        if self.walk_target_dist < 1:
-            state = WalkerBase.calc_state(self)  # calculate state again, against new flag pos
-            self.potential = self.calc_potential()  # avoid reward jump
-        return state
+        j = np.array([j.current_relative_position() for j in self.ordered_joints], dtype=np.float32).flatten()
+        # even elements [0::2] position, scaled to -1..+1 between limits
+        # odd elements  [1::2] angular speed, scaled to show -1..+1
+        self.joint_speeds = j[1::2]
+        self.joints_at_limit = np.count_nonzero(np.abs(j[0::2]) > 0.99)
+
+        body_pose = self.robot_body.pose()
+        parts_xyz = np.array([p.pose().xyz() for p in self.parts.values()]).flatten()
+        self.body_xyz = (
+            parts_xyz[0::3].mean(), parts_xyz[1::3].mean(),
+            body_pose.xyz()[2])  # torso z is more informative than mean z
+        self.body_rpy = body_pose.rpy()
+        z = self.body_xyz[2]
+        if self.initial_z is None:
+            self.initial_z = z
+        r, p, yaw = self.body_rpy
+        self.walk_target_theta = np.arctan2(self.walk_target_y - self.body_xyz[1],
+                                            self.walk_target_x - self.body_xyz[0])
+        self.walk_target_dist = np.linalg.norm(
+            [self.walk_target_y - self.body_xyz[1], self.walk_target_x - self.body_xyz[0]])
+        angle_to_target = self.walk_target_theta - yaw
+
+        rot_speed = np.array(
+            [[np.cos(-yaw), -np.sin(-yaw), 0],
+             [np.sin(-yaw), np.cos(-yaw), 0],
+             [0, 0, 1]]
+        )
+        vx, vy, vz = np.dot(rot_speed, self.robot_body.speed())  # rotate speed back to body point of view
+
+        more = np.array([z - self.initial_z,
+                         np.sin(angle_to_target), np.cos(angle_to_target),
+                         0.3 * vx, 0.3 * vy, 0.3 * vz,
+                         # 0.3 is just scaling typical speed into -1..+1, no physical sense here
+                         r, p], dtype=np.float32)
+        return np.clip(np.concatenate([more] + [j] + [self.feet_contact]), -5, +5)
+
+    def calc_potential(self):
+        # necessary method for BulletBaseEnv
+        return 0
+
+    def reset(self, bullet_client):
+        full_path = os.path.join(os.path.dirname(__file__), "assets", self.model_xml)
+
+        self._p = bullet_client
+        # print("Created bullet_client with id=", self._p._client)
+        if self.doneLoading == 0:
+            self.ordered_joints = []
+            self.doneLoading = 1
+            if self.self_collision:
+                self.objects = self._p.loadMJCF(full_path,
+                                                flags=pybullet.URDF_USE_SELF_COLLISION | pybullet.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS)
+                self.parts, self.jdict, self.ordered_joints, self.robot_body = self.addToScene(self._p, self.objects)
+            else:
+                self.objects = self._p.loadMJCF(full_path)
+                self.parts, self.jdict, self.ordered_joints, self.robot_body = self.addToScene(self._p, self.objects)
+        self.robot_specific_reset(self._p)
+
+        s = self.calc_state()  # optimization: calc_state() can calculate something in self.* for calc_potential() to use
+
+        return s
