3a8a46356e59e33b19d88e79dabb637fd1d1be12
Alexander Koch <alexander@4koch.de>
2019-11-30 20:05:41 +0100

    change to full reset only the first time

--- gym_parkour/envs/parkour_gym.py
+++ gym_parkour/envs/parkour_gym.py
@@ -6,11 +6,12 @@ from gym_parkour.envs.track_scene import TrackScene
 
 
 class ParkourGym(BaseBulletEnv):
+    foot_ground_object_names = {"floor"}  # to distinguish ground and other objects
+
     def __init__(self, render=False):
         self.target_position_xy = (15, 0)
         self.robot = Humanoid(target_position_xy=self.target_position_xy)
         BaseBulletEnv.__init__(self, self.robot, render)
-        self.camera_x = 0
         self.saved_state_id = None
 
     # Overwrite BaseBulletEnv
@@ -20,29 +21,21 @@ class ParkourGym(BaseBulletEnv):
         return self.scene
 
     def reset(self):
-        print('RESETTING')
         if self.saved_state_id is not None:
-            # print("restoreState self.stateId:",self.stateId)
+            # restore state of pybullet with saved state from first reset
             self._p.restoreState(self.saved_state_id)
+        else:
+            r = BaseBulletEnv._reset(self)
+            self._p.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 0)
 
-        r = BaseBulletEnv._reset(self)
-        self._p.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 0)
-
-        self.parts, self.jdict, self.ordered_joints, self.robot_body = self.robot.addToScene(self._p,
-                                                                                             self.scene.ground_plane_mjcf)
-        self.ground_ids = set([(self.parts[f].bodies[self.parts[f].bodyIndex], self.parts[f].bodyPartIndex) for f in
-                               self.foot_ground_object_names])
-        self._p.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 1)
-        if self.saved_state_id is None:
+            self.parts, self.jdict, self.ordered_joints, self.robot_body = self.robot.addToScene(self._p,
+                                                                                                 self.scene.ground_plane_mjcf)
+            self.ground_ids = set([(self.parts[f].bodies[self.parts[f].bodyIndex], self.parts[f].bodyPartIndex) for f in
+                                   self.foot_ground_object_names])
+            self._p.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 1)
             self.saved_state_id = self._p.saveState()
-        print("saving state self.saved_state_id:", self.saved_state_id)
-        return self.robot.calc_state(self.target_position_xy)
 
-    electricity_cost = -2.0 * 4.25  # cost for using motors -- this parameter should be carefully tuned against reward for making progress, other values less improtant
-    stall_torque_cost = -0.1 * 4.25  # cost for running electric current through a motor even at zero rotational speed, small
-    foot_collision_cost = -1.0  # touches another leg, or other objects, that cost makes robot avoid smashing feet into itself
-    foot_ground_object_names = set(["floor"])  # to distinguish ground and other objects
-    joints_at_limit_cost = -0.1  # discourage stuck joints
+        return self.robot.calc_state(self.target_position_xy)
 
     def step(self, a):
         self.robot.apply_action(a)
@@ -53,13 +46,6 @@ class ParkourGym(BaseBulletEnv):
         if distance_to_target < 1 or not self.robot.is_alive():
             done = True
 
-        state = self.robot.calc_state(self.target_position_xy)  # also calculates self.joints_at_limit
+        state = self.robot.calc_state(self.target_position_xy)
         reward = self.robot.calc_reward(a, self.ground_ids)
         return state, reward, bool(done), {}
-
-    def camera_adjust(self):
-        # useless?
-        pass
-        # x, y, z = self.body_xyz
-        # self.camera_x = 0.98 * self.camera_x + (1 - 0.98) * x
-        # self.camera.move_and_look_at(self.camera_x, y - 2.0, 1.4, x, y, 1.0)
