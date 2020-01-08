b48834d303261915e03b2cadd139a27cff2984ff
Alexander Koch <alexander@4koch.de>
2020-01-08 14:28:33 +0100

    increase min body height for alive flag.
    >=0.8 is too high for some good policies

--- gym_parkour/envs/humanoid.py
+++ gym_parkour/envs/humanoid.py
@@ -152,7 +152,7 @@ class Humanoid(ParkourRobot, XmlBasedRobot):
     def is_alive(self):
         body_pitch = self.body_rpy[1]  # not a good predictor
         body_height = self.body_xyz[2]
-        if body_height < 0.8:
+        if body_height < 0.7:
             return False
         else:
             return True
